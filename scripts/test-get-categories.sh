fed4db47c2a84b306647dcde840beeca862cf116
Kalev Alpernas <kalev.alp@gmail.com>
2017-10-29 16:50:06 -0700

    Added scripts for testing catalog API functions.

---
+++ scripts/test-get-categories.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+curl -X GET $1'?user=Owen&pass=owenspassword'
+
--- scripts/test-phone-update.sh
+++ scripts/test-phone-update.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST --data '{ "schema": "com.nordstrom/user-info/update-phone/1-0-0", "id": "654321", "phone": "5551231234", "origin": "hello-retail/test-script-update-phone/testid/testname" }' $1
+curl -X POST --data '{ "schema": "com.nordstrom/user-info/update-phone/1-0-0", "id": "654321", "phone": "5551231234", "origin": "hello-retail/test-script-update-phone/testid/testname", "user": "Owen", "pass": "owenspassword" }' $1
