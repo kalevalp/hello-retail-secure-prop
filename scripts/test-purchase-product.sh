e589b3362289def2f303515e9f323d0ec332c3c0
Kalev Alpernas <kalev.alp@gmail.com>
2017-10-29 23:37:39 -0700

    Changed user/pass in purchase test

--- scripts/test-purchase-product.sh
+++ scripts/test-purchase-product.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST --data '{"user": "Bob", "pass": "bobspassword", "id": "1234567890", "storeCC" : true, "creditCard" : "1234123412341234", "requestId": "0000001"}' $1
+curl -X POST --data '{"user": "Owen", "pass": "owenspassword", "id": "1234567890", "storeCC" : true, "creditCard" : "1234123412341234", "requestId": "0000001"}' $1
